97ce5138e33fcec543c9955ae08b4170dfde33a5
Tom Nagengast <tnagengast@gmail.com>
2024-02-22 00:38:39 +0000

    Vite working

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            protocol: 'wss',
+            host: process.env.APP_URL
+        },
+    },
 });
